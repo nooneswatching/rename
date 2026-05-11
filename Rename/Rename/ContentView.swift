6c9a015df0946972ddf069cf8ae95451034d7db6
Joshua Newton <josh.newton@gmail.com>
2026-05-11 15:13:51 -0400

    chore: scaffold Xcode project (SwiftUI, macOS 13, XCTest)

---
+++ Rename/Rename/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rename
+//
+//  Created by Joshua Newton on 5/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rename/Rename/RenameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenameApp.swift
+//  Rename
+//
+//  Created by Joshua Newton on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct RenameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
